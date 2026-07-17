423aad7d337946299ed71ef3acebbdd1a0121751
SOV710 <chris916911179@outlook.com>
2026-07-17 00:03:33 +0000

    feat(sov-ui-dashboard.el): add page-break-lines, projectile, and all-the-icons packages as dependencies for dashboard

--- lisp/sov-ui-dashboard.el
+++ lisp/sov-ui-dashboard.el
@@ -1,5 +1,20 @@
 ;;; sov-ui-dashboard.el --- Dashboard configuration -*- lexical-binding: t; -*-
 
+(use-package page-break-lines
+  :ensure (:host github
+           :repo "purcell/page-break-lines"
+           :wait t))
+
+(use-package projectile
+  :ensure (:host github
+           :repo "bbatsov/projectile"
+           :wait t))
+
+(use-package all-the-icons
+  :ensure (:host github
+           :repo "domtronn/all-the-icons.el"
+           :wait t))
+
 (use-package dashboard
   :ensure (:host github
            :repo "emacs-dashboard/emacs-dashboard"
