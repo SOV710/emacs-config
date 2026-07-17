cd25f18033c61b1277238bd330a904ee57adb1ec
SOV710 <chris916911179@outlook.com>
2026-07-17 00:22:32 +0000

    feat(sov-ui): add indent-bars package for visual indentation guides
    Add indent-bars to display indentation guides in programming modes, with custom colors and depth-based highlighting.

--- lisp/sov-ui.el
+++ lisp/sov-ui.el
@@ -14,5 +14,23 @@
 (require 'sov-ui-dashboard)
 (require 'sov-ui-modeline)
 
+(use-package indent-bars
+  :ensure (:host github
+           :repo "jdtsmith/indent-bars"
+           :wait t)
+  :hook (prog-mode . indent-bars-mode)
+  :custom
+  (indent-bars-color '(highlight :face-bg t :blend 0.15))
+  (indent-bars-pattern ".")
+  (indent-bars-width-frac 0.1)
+  (indent-bars-pad-frac 0.1)
+  (indent-bars-zigzag nil)
+  (indent-bars-color-by-depth
+   '(:regexp "outline-\\([0-9]+\\)" :blend 1))
+  (indent-bars-highlight-current-depth '(:blend 0.5))
+  (indent-bars-display-on-blank-lines t)
+  (indent-bars-treesit-support t))
+
+
 (provide 'sov-ui)
 ;;; sov-ui.el ends here
